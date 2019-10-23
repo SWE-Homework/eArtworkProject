123615ce66d48211e8104ebf4c6b01796e49954a
Bisrat Tekle <37101529+bisur@users.noreply.github.com>
2019-10-23 17:29:47 -0500

    Mockito Unit  test for User account

--- src/main/java/com/artwork/online/eartwork/model/UserAccount.java
+++ src/main/java/com/artwork/online/eartwork/model/UserAccount.java
@@ -4,7 +4,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+
 import javax.validation.constraints.Size;
 import java.util.*;
 
@@ -48,6 +48,15 @@ public class UserAccount {
     @Column(name = "role_id")
     private List <RoleUser> roleUsers;
 
+    public UserAccount(@NotBlank(message = "* First Name is required") String firstName,
+                       @NotBlank(message = "* Last Name is required") String lastName, @NotBlank(message = "* Email is required") String email, @Size(min = 8) @NotBlank(message = "* password is required") String password, String loginStatus, List<RoleUser> roleUsers) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.loginStatus = loginStatus;
+        this.roleUsers = roleUsers;
+    }
 
     public long getUserAccountId() {
         return userAccountId;
