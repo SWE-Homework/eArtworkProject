0792590bcafff62751a00a82a7af4c2f14542e68
ecadet-mum <51096249+ecadet-mum@users.noreply.github.com>
2019-10-23 21:49:27 -0400

    Adding api access for adding/updating/deleting an artwork

--- src/main/java/com/artwork/online/eartwork/controller/FileUploadRestController.java
+++ src/main/java/com/artwork/online/eartwork/controller/FileUploadRestController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost"}, allowedHeaders = "*")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost","http://localhost:4201"}, allowedHeaders = "*")
 @RequestMapping("/eartwork/api")
 public class FileUploadRestController {
 
--- src/test/java/com/artwork/online/eartwork/EartworkApplicationTests.java
+++ src/test/java/com/artwork/online/eartwork/EartworkApplicationTests.java
@@ -3,15 +3,14 @@ package com.artwork.online.eartwork;
 import com.artwork.online.eartwork.model.RoleUser;
 import com.artwork.online.eartwork.model.UserAccount;
 import com.artwork.online.eartwork.service.UserAccountService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EartworkApplicationTests {
     @Autowired
